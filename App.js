aea6e57ebd91bb571bdae3a277d1678cbab26cf6
maheshsalunkhe2510 <maheshsalunkhe.it@gmail.com>
2024-02-04 21:23:08 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom"
 
-const   = ReactDOM.createRoot(document.getElementById("root"));
-const parrent = React.createElement("div", { id: "parrent" },
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const parent = React.createElement("div", { id: "parent" },
     [React.createElement("div", { id: "child" }), [
-        React.createElement("h1", { id: "heading" }, "I smh1 tag"),
+        React.createElement("h1", { id: "heading" }, "I  smh1 tag test"),
         React.createElement("h2", { id: "heading" }, "I smh1 tag")], React.createElement("div", { id: "child1" }), [
         React.createElement("h1", { id: "heading" }, "I smh1 tag1"),
         React.createElement("h2", { id: "heading" }, "I smh1 tag2")]]);
-root.render(parrent);
+root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 #Namaste React
+
+# parcel
+-Dev build 
+-local server
+-file watching algorithm - written in c++
+-caching -faster build
+-image optimization 
+-minification
+-bundling 
+-file compressing
+-consisting hashing 
+-code splitting 
+-differential building -support older browser
+-Error hading
+-HTTPs
+-Diagnostics
