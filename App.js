449e69f3393b415c00ee467487a4537f48fba946
maheshsalunkhe2510 <maheshsalunkhe.it@gmail.com>
2024-02-04 23:07:26 +0530

    JSX babel

--- App.js
+++ App.js
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const parent = React.createElement("div", { id: "parent" },
-    [React.createElement("div", { id: "child" }), [
-        React.createElement("h1", { id: "heading" }, "I  smh1 tag test"),
-        React.createElement("h2", { id: "heading" }, "I smh1 tag")], React.createElement("div", { id: "child1" }), [
-        React.createElement("h1", { id: "heading" }, "I smh1 tag1"),
-        React.createElement("h2", { id: "heading" }, "I smh1 tag2")]]);
-root.render(parent);
+
+        const jsxHeading=<h1 id="heading">Namaste React using</h1>
+root.render(jsxHeading);
