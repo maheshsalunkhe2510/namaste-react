cfd4108fb6eeae34fdcd0b54611158a489fac367
maheshsalunkhe2510 <maheshsalunkhe.it@gmail.com>
2024-02-08 22:42:54 +0530

    Episode-06

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,21 +1,46 @@
 import RestaurantCard from "./RestaurantCard"
-import restaurantsList from '../utils/mockData';
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 
 const Body = () => {
+    const [restaurantsListData, setRestaurantsListData] = useState([]);
+    const [searchInputText, setSearchInputText] = useState([]);
+    const [filterRestaurant, setFilterRestaurant] = useState([]);
+    useEffect(() => {
+        console.log('use effect is called');
+        fetchData();
+    }, []);
 
-    const [restaurantsListData, setRestaurantsListData] = useState(restaurantsList);
-
+    fetchData = async () => {
+        data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.183561174748895&lng=72.99311242997646&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING", {
+        });
+        let res = await data.json()
+        console.log(res);
+        const resData = res.data.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+        setRestaurantsListData(resData);
+        setFilterRestaurant(resData);
+    };
     return <div className="body">
+        <div className="searchBar">
+            <input className="search-input" value={searchInputText} onChange={(event) => {
+                setSearchInputText(event.target.value);
+            }} />
+            <button className="search-btn" onClick={() => {
+                let filteredList = restaurantsListData.filter(res => res.info.name.toLowerCase().includes(searchInputText.toLowerCase()));
+                setFilterRestaurant(filteredList);
+            }}>
+                search
+            </button>
+        </div>
         <div className="filter">
             <button onClick={() => {
-                let filteredList = restaurantsListData.filter(res=>res.info.avgRating>4.5);
-                setRestaurantsListData(filteredList);
+                let filteredList = restaurantsListData.filter(res => res.info.avgRating > 4.5);
+                setFilterRestaurant(filteredList);
             }} className="filter-btn" >To rated button </button>
         </div>
         <div className="res-container">
             {
-                restaurantsListData.map(restaurant => <RestaurantCard key={restaurant.id} resData={restaurant.info} />)
+                filterRestaurant.map(restaurant => <RestaurantCard key={restaurant.id} resData={restaurant.info} />)
             }
         </div>
     </div>
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { CDN_URL } from "../utils/constants"
 const Header = () => {
+    const [loginBtnText, setLoginBtnText] = useState('login');
     return <div className="header">
         <div className="logo-container">
             <img src={CDN_URL}></img>
@@ -10,6 +12,9 @@ const Header = () => {
                 <li>About us</li>
                 <li>Contact Us</li>
                 <li>Cart</li>
+                <li><button className="login-btn" onClick={() => {
+                    loginBtnText === "login" ? setLoginBtnText('logout') : setLoginBtnText('login');
+                }}>{loginBtnText}</button></li>
             </ul>
         </div>
     </div>
